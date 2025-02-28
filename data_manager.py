7dd78c0024af69a59c7c195abe0bb63e5da585d9
Bhupesh0007 <127049527+Bhupesh0007@users.noreply.github.com>
2025-02-28 10:22:41 +0530

    Add files via upload

---
+++ data_manager.py
@@ -0,0 +1,37 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/4e1252bfdc3d54a40d55648baab1d285/flightCalculator/prices"
+
+
+class DataManager:
+
+    def __init__(self):
+        self._user = "bhupesh7"
+        self._password = "bhupesh@1509"
+        self._authorization = HTTPBasicAuth(self._user, self._password)
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        # Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        # Try importing pretty print and printing the data out again using pprint() to see it formatted.
+        # pprint(data)
+        return self.destination_data
+
+    # In the DataManager Class make a PUT request and use the row id from sheet_data
+    # to update the Google Sheet with the IATA codes. (Do this using code).
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
